efb3c50680b06767e37652cace31a87a2a8aa714
Eman Abbas <eman.abbas@autotrader.co.uk>
2026-06-05 14:40:11 +0100

    Added browser routes to the front end

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,45 @@
 import CreateRecipeForm from "./components/CreateRecipeForm.tsx";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Link} from "react-router-dom";
+
 
 export default function App() {
-  return (
-      <>
-          <div>
-              <h1>Recipe Book</h1>
-          </div>
-          <div>
-              <h2>Add your recipe below</h2>
-          </div>
-          <div>
-              <CreateRecipeForm/>
-          </div>
-      </>
-  );
+    return (
+        <BrowserRouter>
+            <>
+                <Routes>
+                    <Route
+                        path={"/"}
+                        element={
+                        <>
+                            <h1>Recipe Book</h1>
+                            <Link to="/createRecipe">
+                                <button>Go to create recipe</button>
+                            </Link>
+                            <Link to="/allRecipes">
+                                <button>View all recipes</button>
+                            </Link>
+                        </>
+                        }
+                    />
+                    <Route
+                        path="/createRecipe"
+                        element={
+                            <>
+                                <h2>Add your recipe</h2>
+                                <CreateRecipeForm/>
+                            </>
+                        }
+                        />
+                    <Route
+                        path="/allRecipes"
+                        element={
+                        <>
+                        </>
+                        }
+                    />
+                </Routes>
+            </>
+        </BrowserRouter>
+    )
 }
