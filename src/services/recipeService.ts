cdc3e6112d96ea18447e68c4014eec52e5c92768
Eman Abbas <eman.abbas@autotrader.co.uk>
2026-06-13 00:29:35 +0100

    added edit and delete functionality to the service

--- src/services/recipeService.ts
+++ src/services/recipeService.ts
@@ -14,6 +14,32 @@ export async function createRecipe(recipe: Recipe) {
     return response.json();
 }
 
+export async function editRecipe(recipe: Recipe) {
+    const response = await fetch("http://localhost:8080/recipes",{
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(recipe),
+    });
+    if (!response.ok) {
+        throw new Error("Failed to create recipe");
+    }
+
+    return response.json();
+}
+
+export async function deleteRecipe(id : number) {
+    const response = await fetch(`http://localhost:8080/${id}`, {
+        method: "DELETE"
+    })
+    if (!response.ok) {
+        throw new Error("Failed to create recipe");
+    }
+
+    return id;
+}
+
 export async function viewRecipes() {
     const response = await fetch("http://localhost:8080/recipes", {
         method: "GET"
