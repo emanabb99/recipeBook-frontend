d33d22babb2ebe3a41098121a41e9e908a81c62b
Eman Abbas <eman.abbas@autotrader.co.uk>
2026-06-13 21:31:59 +0100

    Fixed commit issues

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,6 @@
 import {useState} from "react";
 import {getUser} from "../services/userService.ts";
-import HomePage from "./HomePage.tsx";
-import {useNavigate,Link} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import type {User} from "../types/User.ts";
 
 interface SuccessProp {
@@ -12,14 +11,12 @@ export default function Login({onLoginSuccess}: SuccessProp) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
-    const [success, setSuccess] = useState(false);
     const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
             const user = await getUser(email, password);
-            setSuccess(true);
             onLoginSuccess(user);
             navigate("/homePage");
 
