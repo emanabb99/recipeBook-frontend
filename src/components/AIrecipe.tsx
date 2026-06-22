ee626c807aedb49adb49b77ebb6efdf6611bb7d4
Eman Abbas <eman.abbas@autotrader.co.uk>
2026-06-22 16:53:47 +0100

    Added an add button that needs hooking up and customised UX

--- src/components/AIrecipe.tsx
+++ src/components/AIrecipe.tsx
@@ -3,8 +3,8 @@ import {sendPrompt} from "../services/aiService.ts";
 import type {Recipe} from "../types/Recipe.ts";
 
 export default function AIrecipe() {
-    const [generatedRecipe,setGeneratedRecipe] = useState<Recipe | null>(null);
-    const [prompt,setPrompt] = useState("");
+    const [generatedRecipe, setGeneratedRecipe] = useState<Recipe | null>(null);
+    const [prompt, setPrompt] = useState("");
 
     const handlePrompt = async (e) => {
         e.preventDefault();
@@ -16,24 +16,31 @@ export default function AIrecipe() {
         <>
             <h2>Let AI help you!</h2>
             <textarea value={prompt}
-                      onChange={(e)=>
+                      onChange={(e) =>
                           setPrompt(e.target.value)
                       }
                       placeholder="Enter prompt for recipe"/>
             <div>
-            <button
-                onClick={handlePrompt}
-            >Generate recipe!</button>
+                <button
+                    onClick={handlePrompt}
+                >Generate recipe!
+                </button>
             </div>
             {generatedRecipe && (
                 <>
-                    <p>{generatedRecipe.name}</p>
-                    {generatedRecipe.ingredients.map((ingredient)=>(
-                        <li>{ingredient}</li>
-                    ))}
-                    {generatedRecipe.instructions.map((instruction)=>(
-                        <p>{instruction}</p>
-                    ))}
+                    <div align="center">
+                        <h3>AI generated recipe:</h3>
+                        <p>{generatedRecipe.name}</p>
+                        {generatedRecipe.ingredients.map((ingredient) => (
+                            <li>{ingredient}</li>
+                        ))}
+                        {generatedRecipe.instructions.map((instruction) => (
+                            <p>{instruction}</p>
+                        ))}
+                        <div>
+                            <button type="button">Add Recipe</button>
+                        </div>
+                    </div>
                 </>
             )}
         </>
